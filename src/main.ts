bfdb8a8b5c4efa4c22948fb489652726d054a2ca
xakoy <xakoy@qq.com>
2020-04-22 11:17:54 +0800

    更新组件

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import 'element-ui/lib/theme-chalk/index.css'
 import 'belvoly-form-studio/dist/belvoly-form-studio.css'
 import { initControls } from './components/controls'
 
+// import BFS from 'belvoly-form-studio'
+// Vue.use(BFS, { isUseDefaultControls: false })
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 initControls()
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App)
 }).$mount('#app')
